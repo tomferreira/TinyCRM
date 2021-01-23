28c273919315434e391926a25a47fa8f339e10b0
cirillo.ferreira <cirillo.ferreira@gmail.com>
2021-01-23 15:21:32 -0300

    Use stisla UI template for Bootstrap

--- TinyCRM.Web.MVC/Controllers/HomeController.cs
+++ TinyCRM.Web.MVC/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using TinyCRM.Web.MVC.Models;
 
 namespace TinyCRM.Web.MVC.Controllers
@@ -19,11 +15,6 @@ namespace TinyCRM.Web.MVC.Controllers
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
