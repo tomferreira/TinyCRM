d90183031ca33e072141f1d420a0a9f1afe23a7f
cirillo.ferreira <cirillo.ferreira@gmail.com>
2021-01-23 10:09:35 -0300

    Register injection of dependencies and startup DBContext at main project

---
+++ TinyCRM.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TinyCRM.Application.Interfaces;
+using TinyCRM.Application.Services;
+using TinyCRM.Domain.Interfaces;
+using TinyCRM.Infra.Data.Repositories;
+
+namespace TinyCRM.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<INaturalPersonService, NaturalPersonService>();
+            services.AddScoped<ILegalPersonService, LegalPersonService>();
+
+            services.AddScoped<INaturalPersonRepository, NaturalPersonRepository>();
+            services.AddScoped<ILegalPersonRepository, LegalPersonRepository>();
+        }
+    }
+}
